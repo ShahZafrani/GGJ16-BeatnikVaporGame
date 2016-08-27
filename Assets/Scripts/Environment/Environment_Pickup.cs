2f1216ff32a2be6cc28f3c5f62fe98cdc530bde3
Henry Moss <dltech976@gmail.com>
2016-08-26 22:10:44 -0400

    import unity project

---
+++ Assets/Scripts/Environment/Environment_Pickup.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Environment_Pickup : MonoBehaviour {
+
+    public bool isShard;
+
+    public Transform textField;
+    
+    // Use this for initialization
+	void Start () {
+        textField.gameObject.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //textField.gameObject.SetActive(false);
+	}
+
+    void OnTriggerEnter ( Collider col)
+    {
+        if (col.tag == "Player")
+        {
+            if (isShard)
+            {
+                //destroy this
+                //do GUI stuff
+                Destroy(this);
+            }
+            
+            else
+            {
+                //don't destroy this
+                //turn on GUI text
+                textField.gameObject.SetActive(true);
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if (col.tag == "Player")
+        {
+            if (!isShard)
+            {
+                //if leave trigger, disable GUI text
+                textField.gameObject.SetActive(false);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Player/Player_Movement.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_Movement : MonoBehaviour {
+
+    public GAME_STATE states;
+    public enum GAME_STATE
+    {
+        PLAY = 0,
+        INTERACT = 1,
+        WAIT = 2
+    }
+
+    public bool canMove;
+    public float speed;
+    public float waitTime;
+    public float interactTime;
+
+    private float horizontalMove;
+    private float verticalMove;
+
+    // Use this for initialization
+    void Start () {
+        StartCoroutine(CreateFSM());
+    }
+
+    IEnumerator CreateFSM()
+    {
+        while (true)
+        {
+            yield return StartCoroutine(states.ToString());
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+
+    IEnumerator PLAY()
+    {
+        if (canMove)
+        {
+            yield return new WaitForSeconds(0);
+            //do movement logic here
+            //just use ifs. smoother movement
+            Vector3 movement = new Vector3();
+
+            if (Input.GetAxisRaw("Vertical") > 0)
+            {
+                //moveStates = MOVE_STATE.UP;
+                movement.z += speed;
+            }
+
+            if (Input.GetAxisRaw("Vertical") < 0)
+            {
+                //moveStates = MOVE_STATE.DOWN;
+                movement.z -= speed;
+            }
+            if (Input.GetAxisRaw("Horizontal") < 0)
+            {
+                //moveStates = MOVE_STATE.LEFT;
+                movement.x -= speed;
+            }
+            if (Input.GetAxisRaw("Horizontal") > 0)
+            {
+                //moveStates = MOVE_STATE.RIGHT;
+                movement.x += speed;
+            }
+            if (Input.GetAxisRaw("Horizontal") == 0 && Input.GetAxisRaw("Vertical") == 0)
+            {
+                //moveStates = MOVE_STATE.IDLE;
+                movement = new Vector3(0, 0, 0);
+            }
+
+            transform.position += movement * Time.deltaTime;
+        }
+    }
+
+    IEnumerator INTERACT()
+    {
+        yield return new WaitForSeconds(interactTime);
+        //make player wait/perform interaction with pickup
+        //maybe just an ontriggerstay?
+    }
+
+    IEnumerator WAIT()
+    {
+        canMove = false;
+        yield return new WaitForSeconds(waitTime);
+        canMove = true;
+        states = GAME_STATE.PLAY;
+    }
+}
