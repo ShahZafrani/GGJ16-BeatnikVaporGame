07bf3bd00a872508a1cd93a221f56d0ce8442a73
Henry Moss <dltech976@gmail.com>
2016-08-28 12:55:51 -0400

    we finished, yo

--- Assets/Scripts/System/System_EndGame.cs
+++ Assets/Scripts/System/System_EndGame.cs
@@ -3,20 +3,57 @@ using System.Collections;
 
 public class System_EndGame : MonoBehaviour {
 
+    public bool doThis;
+
     public System_Fade systemFade;
 
     public Transform finalQuote;
 
-	// Use this for initialization
-	void Start () {
-        finalQuote.gameObject.SetActive(false);
+    public GAME_STATE states;
+    public enum GAME_STATE
+    {
+        PLAY = 0,
+        TRIGGERED = 1,
+        COMPLETE = 2
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Use this for initialization
+    void Start () {
+        finalQuote.gameObject.SetActive(false);
+        doThis = false;
+        StartCoroutine(CreateFSM());
+    }
+
+    IEnumerator CreateFSM()
+    {
+        while (true)
+        {
+            yield return StartCoroutine(states.ToString());
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
 	    if (systemFade.states == System_Fade.SMUDGE_STATE.VISIBLE)
         {
-            finalQuote.gameObject.SetActive(true);
+            states = GAME_STATE.TRIGGERED;
         }
 	}
+
+    IEnumerator PLAY()
+    {
+        yield return new WaitForSeconds(0);
+    }
+
+    IEnumerator TRIGGERED()
+    {
+        yield return new WaitForSeconds(1.5f);
+        finalQuote.gameObject.SetActive(true);
+        states = GAME_STATE.COMPLETE;
+    }
+
+    IEnumerator COMPLETE()
+    {
+        yield return new WaitForSeconds(0);
+    }
 }
---
+++ Assets/Scripts/animateDirections.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class animateDirections : MonoBehaviour
+{
+	private Animator anim;
+	// Use this for initialization
+	void Start ()
+	{
+		anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		float lastInputX = Input.GetAxis ("Horizontal");
+		float lastInputY = Input.GetAxis ("Vertical");
+
+		if (lastInputX != 0 || lastInputY != 0) {
+			anim.SetBool ("Walking", true);
+
+			if (lastInputX > 0)
+            {
+				anim.SetInteger ("Direction", 4);
+                Debug.Log("right");
+			}
+
+            if (lastInputX < 0)
+            {
+				anim.SetInteger("Direction", 3);
+                Debug.Log("left");
+            } 
+
+			if (lastInputY > 0)
+            {
+				anim.SetInteger ("Direction", 1);
+                Debug.Log("up");
+            }
+
+            if (lastInputY < 0)
+            {
+				anim.SetInteger("Direction", 2);
+                Debug.Log("down");
+            } 
+
+		}
+
+        else
+        {
+			anim.SetBool ("Walking", false);
+            Debug.Log("idle");
+        }
+	}
+}
+
